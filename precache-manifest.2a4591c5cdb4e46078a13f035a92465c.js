dadd9803ba0d3510d786c927c229f03dafef4b51
Abhishek Srivastava <abhishek.srivastava1@srmtechsol.com>
2019-01-25 18:12:52 +0530

    Updates

---
+++ precache-manifest.2a4591c5cdb4e46078a13f035a92465c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firstreactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4800bb648a648568ca77",
+    "url": "/firstreactapp/static/js/runtime~main.4800bb64.js"
+  },
+  {
+    "revision": "7e9f8635185fd3cb4fa8",
+    "url": "/firstreactapp/static/js/main.7e9f8635.chunk.js"
+  },
+  {
+    "revision": "f50debf22cceb166786e",
+    "url": "/firstreactapp/static/js/1.f50debf2.chunk.js"
+  },
+  {
+    "revision": "7e9f8635185fd3cb4fa8",
+    "url": "/firstreactapp/static/css/main.9cdea60b.chunk.css"
+  },
+  {
+    "revision": "f50debf22cceb166786e",
+    "url": "/firstreactapp/static/css/1.4ff941cd.chunk.css"
+  },
+  {
+    "revision": "0a77b1065d496e7c6ce5ef825816e754",
+    "url": "/firstreactapp/index.html"
+  }
+];
